67e4fbf64a8b639c86f6b5e95f32da7c502f26d2
rijoalvi@gmail.com <rijoalvi@gmail.com@5c4c3386-4ac9-2bb3-2425-1b8d11fe38ad>
2011-10-24 23:34:03 +0000

    Con main

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,1 +1,28 @@
+#include "matrix.h"
+#include "color.h"
+#include "aabb.h"
+#include "camera.h"
+#include "defines.h"
+#include "diffuse.h"
+#include "image.h"
+#include "intersectable.h"
+#include "intersection.h"
+#include "listaccelerator.h"
+#include "material.h"
+#include "mesh.h"
+#include "node.h"
+#include "pointlight.h"
+#include "primitive.h"
+#include "ray.h"
+#include "rayaccelerator.h"
+#include "raytracer.h"
+#include "scene.h"
+#include "sphere.h"
+#include "timer.h"
+#include "triangle.h"
+#include "whittedtracer.h"
 
+
+int main(){
+    return(0);
+}
